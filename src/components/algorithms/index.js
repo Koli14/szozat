bb430e5269449ea7ff595e40c4b609d022789282
Karácsony Kolos <koli14@gmail.com>
2022-03-28 21:11:07 +0200

    Fix present filter

--- src/components/algorithms/index.js
+++ src/components/algorithms/index.js
@@ -12,14 +12,12 @@ const AlgoContainer = ({ guesses, algo }) => {
   const filteredWords = words
     .filter(word => word.every(letter => charStatuses[letter] !== "absent"))  // Filter words containing absent letters
     .filter(word => {
-      let shouldFilter = true
       for (const [char, status] of Object.entries(charStatuses)) {
-        if(status === "present" ) {
-          
+        if(status === "present" && !word.includes(char) ) {
+          return false
         }
-        return shouldFilter
       }
-        
+      return true
       // word.some(letter => charStatuses[letter] === "present")) 
     } // Filter words not containing present letter
   // .filter(word => word.some(letter => {})) // Filter words not having correct character at correct place
