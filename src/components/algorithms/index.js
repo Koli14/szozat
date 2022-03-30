f0df8ad77099459154c6b0d991324260d6723895
koli <koli14@gmail.com>
2022-03-30 22:16:38 +0200

    beginning - not complete

--- src/components/algorithms/index.js
+++ src/components/algorithms/index.js
@@ -8,40 +8,45 @@ import algo2 from './algo2'
 import algo3 from './algo3'
 
 const AlgoContainer = ({ guesses, algo, withPosition }) => {
+  createBigStats()
   const charStatuses = getStatuses(guesses)
   const [word, setWord] = useState('')
   useEffect(() => {
-    const filteredWords = words
-      .filter(word => word.every(letter => charStatuses[letter] !== 'absent'))  // Filter words containing absent letters
-      .filter(word => {
-        for (const [char, status] of Object.entries(charStatuses)) {
-          if (status === 'present' && !word.includes(char)) {
-            return false
-          }
-        }
-        return true
-      }) // Filter words not containing present letter
-      .filter(word => {
-        let shouldKeep = true
-        guesses.forEach(guess => {
-          const guessStatuses = getGuessStatuses(guess)
-          guessStatuses.forEach((guessStatus, index) => {
-            if (guessStatus === 'correct' && guess[index] !== word[index]) {
-              shouldKeep = false
-              return false
-            }
-            if (guessStatus === 'present' && guess[index] === word[index]) {
-              shouldKeep = false
-              return false
-            }
-          })
-        })
-        return shouldKeep
-      }) // Filter words not having correct character at correct place, or having present letter incorrect place
+    const filteredWords = filterWords(charStatuses, guesses)
     const stats = createLetterFrequencyObject(filteredWords, CHAR_VALUES, withPosition)
     setWord(algo(filteredWords, stats));
   }, [charStatuses])
   return <div className='text-sm text-gray-500 dark:text-gray-300'>{word}</div>
+}
+
+const filterWords = (charStatuses, guesses) => {
+  return words
+    .filter(word => word.every(letter => charStatuses[letter] !== 'absent'))  // Filter words containing absent letters
+    .filter(word => {
+      for (const [char, status] of Object.entries(charStatuses)) {
+        if (status === 'present' && !word.includes(char)) {
+          return false
+        }
+      }
+      return true
+    }) // Filter words not containing present letter
+    .filter(word => {
+      let shouldKeep = true
+      guesses.forEach(guess => {
+        const guessStatuses = getGuessStatuses(guess)
+        guessStatuses.forEach((guessStatus, index) => {
+          if (guessStatus === 'correct' && guess[index] !== word[index]) {
+            shouldKeep = false
+            return false
+          }
+          if (guessStatus === 'present' && guess[index] === word[index]) {
+            shouldKeep = false
+            return false
+          }
+        })
+      })
+      return shouldKeep
+    }) // Filter words not having correct character at correct place, or having present letter incorrect place
 }
 
 const createLetterFrequencyObject = (words, chars, withPosition) => {
@@ -66,4 +71,31 @@ const Algo1 = ({ guesses }) => <AlgoContainer guesses={guesses} algo={algo1} />
 const Algo2 = ({ guesses }) => <AlgoContainer guesses={guesses} algo={algo2} withPosition />
 const Algo3 = ({ guesses }) => <AlgoContainer guesses={guesses} algo={algo3} />
 
+const createBigStats = () => {
+  const stats = {}
+  for (let index = 0; index < 1000; index++) {
+    const { solution } = getWordForDay(index)
+    const charStatuses = getStatuses([])
+
+  }
+}
+
+const getWordForDay = (i) => {
+  // January 1, 2022 Game Epoch
+  const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
+  const msInDay = 86400000
+  const now = epochMs + (i * msInDay)
+  const index = Math.floor((now - epochMs) / msInDay)
+  const indexModulo = index % WORDS.length
+  const nextday = (index + 1) * msInDay + epochMs
+
+  return {
+    solution: WORDS[indexModulo],
+    solutionIndex: indexModulo,
+    tomorrow: nextday,
+  }
+}
+
+
 export { Algo1, Algo2, Algo3 }
+
